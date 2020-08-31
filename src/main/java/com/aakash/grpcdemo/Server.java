3f7cb7b3cb5459706eb97b24e2943b8b824ca734
Aakash Kharul <kharulaakash@gmail.com>
2020-08-31 17:52:18 +0530

    added gitignore

---
+++ src/main/java/com/aakash/grpcdemo/Server.java
@@ -0,0 +1,12 @@
+package com.aakash.grpcdemo;
+
+import com.aakash.grpcdemo.generated.TodoServiceGrpc.TodoServiceImplBase;
+
+public class Server {
+	public static void main(String[] args) {
+		
+	}
+	private class TodoServiceImpl extends TodoServiceImplBase {
+		
+	}
+}
